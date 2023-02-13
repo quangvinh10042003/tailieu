daea53013b54b4bdb8f153243b9190fe5d52524b
Quang Vinh <qvinhdx@gmail.com>
2023-02-13 11:09:49 +0700

    xong

--- nodejs/server.js
+++ nodejs/server.js
@@ -8,9 +8,19 @@ server.use(bodyParser.json()); //. api
 server.use(cors());
 const conn = require('./connect');
 server.use(express.static('public'));
+// nếu cần
+const ejs = require('ejs');
+server.set('view engine', 'html');
+server.engine('html', ejs.renderFile);
+
 
 require('./routers/home')(server);
-
+// nếu làm bằng view engine
+// module.exports = function(server) {
+//     server.get('/admin', function(req, res) {
+//         res.render('admin/index')
+//     });
+// }
 server.listen(3000, function() {
     console.log('Server listening on port  http://localhost:3000');
 })
